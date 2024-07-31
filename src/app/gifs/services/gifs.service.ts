03f52284345eda1e54a5afb1b8bd9f174d956dc7
Pierre Carrillo <pierre.andres@bootcamp-upgrade.com>
2024-07-31 12:27:48 +0200

    deshabilitando localStorage

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -14,7 +14,7 @@ export class GifsService {
   private serviceUrl: string = "https://api.giphy.com/v1/gifs";
 
   constructor(private http: HttpClient) {
-    this.loadLocalStorage();
+    // this.loadLocalStorage();
     console.log("Gifs Service Ready");
    }
 
@@ -31,20 +31,20 @@ export class GifsService {
 
     this._tagHistory.unshift( tag );
     this._tagHistory = this._tagHistory.splice(0,10);
-    this.saveLocalStorage();
+    // this.saveLocalStorage();
   }
 
   private saveLocalStorage(): void {
     localStorage.setItem("history", JSON.stringify( this._tagHistory ));
   }
 
-  private loadLocalStorage():void {
-    if( !localStorage.getItem("history") ) return;
-    this._tagHistory = JSON.parse( localStorage.getItem("history")! );
+  // private loadLocalStorage():void {
+  //   if( !localStorage.getItem("history") ) return;
+  //   this._tagHistory = JSON.parse( localStorage.getItem("history")! );
 
-    if( this._tagHistory.length === 0 ) return;
-    this.searchTag( this._tagHistory[0] );
-  }
+  //   if( this._tagHistory.length === 0 ) return;
+  //   this.searchTag( this._tagHistory[0] );
+  // }
 
   searchTag( tag:string ):void {
     if( tag.length === 0 ) return;
